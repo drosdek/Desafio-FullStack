e4263f4e7d5ae894846fe2f0687fb40bf3c5887d
Josnei Drosdek <josneidrosdek@gmail.com>
2022-06-08 01:26:31 -0300

    Passando container para page desenvolvedor;

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DesenvolvedoresPage from "./pages/desenvolvedores.page";
 import MainPage from "./pages/main.page";
 import NiveisPage from "./pages/niveis.page";
+import DesenvolvedoresPageContainer from "./container/desenvolvedoresPage.container";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/desenvolvedores" element={<DesenvolvedoresPage />} />
+        <Route path="/desenvolvedores" element={<DesenvolvedoresPageContainer />} />
         <Route path="/niveis" element={<NiveisPage />} />
       </Routes>
     </BrowserRouter>
