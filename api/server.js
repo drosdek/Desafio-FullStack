eddf4dce0b63a2233b5b2dd80f8a2307a230892d
Josnei Drosdek <josneidrosdek@gmail.com>
2022-06-09 23:27:25 -0300

    Implementado log de acordo com ambiente dev ou test;

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var cors = require("cors");
 const bodyParser = require("body-parser");
+var config = require('config');
+var morgan = require('morgan');
 
 const errorHandler = require("./middleware/error-handler");
 
@@ -11,8 +13,18 @@ var desenvolvedoresRouter = require("./routes/desenvolvedores");
 
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+//não mostra o log quando é teste
+if(config.util.getEnv('NODE_ENV') !== 'test') {
+   // usa o morgan para log na linha de comando
+    app.use(morgan('combined')); //'combined' gera os LOGs do estilo Apache
+}
+
+
+// analisa application/json e procura por texto bruto
+app.use(bodyParser.json());                                     
+app.use(bodyParser.urlencoded({extended: true}));               
+app.use(bodyParser.text());                                    
+app.use(bodyParser.json({ type: 'application/json'}));
 app.use(cors());
 
 app.use("/", indexRouter);
